874e651c1fc1a466c3c688688e6d19090aa1d9bd
Naana Shifah <shifahnana2@gmail.com>
2025-07-08 13:41:36 +0300

    feat: Add callback functions and solution to callback hell

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 console.log("Before");
-const user = getUser(1);
-console.log(user);
+getUser(1, handleUser);
 console.log("After");
 
-function getUser(id) {
+function displayCommits(commits) {
+  console.log("Commits:", commits);
+}
+
+function handleRepos(repos) {
+  getCommits(repos[0], displayCommits); // Assuming we only get commits for the first repo
+}
+
+function handleUser(user) {
+  getRepos(user.gitHubUsername, handleRepos);
+}
+
+function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading a user from a database");
-    return { id: id, gitHubUsername: "mosh" };
+    callback({ id: id, gitHubUsername: "mosh" });
   }, 2000);
-  return 1;
+}
+
+function getRepos(username, callback) {
+  setTimeout(() => {
+    console.log("Getting all repositories for", username);
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
+}
+
+function getCommits(repo, callback) {
+  setTimeout(() => {
+    console.log("Getting commits for", repo);
+    callback(["commit1", "commit2"]);
+  }, 2000);
 }
