ea46e3b6cde2469d7daed5f3400b2c9e6d2e1d85
Naana Shifah <shifahnana2@gmail.com>
2025-07-14 13:50:12 +0300

    Use the async/await instead of promises

--- index.js
+++ index.js
@@ -8,11 +8,24 @@ console.log("Before");
 // });
 
 // use promise instead of nesting callbacks
-getUser(1)
-  .then((user) => getRepositories(user.gitHubUsername))
-  .then((repos) => getCommits(repos[0]))
-  .then((commits) => console.log("Commits", commits))
-  .catch((err) => console.log("Error", err.message));
+// getUser(1)
+//   .then((user) => getRepositories(user.gitHubUsername))
+//   .then((repos) => getCommits(repos[0]))
+//   .then((commits) => console.log("Commits", commits))
+//   .catch((err) => console.log("Error", err.message));
+
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (err) {
+    console.log("Error", err.message);
+  }
+}
+
+displayCommits();
 
 console.log("After");
 
@@ -29,7 +42,8 @@ function getRepositories(username) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("Calling GitHub API...");
-      resolve(["repo1", "repo2", "repo3"]);
+      // resolve(["repo1", "repo2", "repo3"]);
+      reject(new Error("Coould not get the repos"));
     }, 2000);
   });
 }
