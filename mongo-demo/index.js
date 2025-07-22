0949e88991a88f35516cbd08dc1874db13994973
Naana Shifah <shifahnana2@gmail.com>
2025-07-22 09:06:52 +0300

    see comparison operators in mongodb

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -34,12 +34,15 @@ async function createCourse() {
 }
 
 async function getCourses() {
-  const courses = await Course.find({ author: "Mosh", isPublished: true })
+  const courses = await Course
+    // .find({ author: "Mosh", isPublished: true })
+    // .find({price: {$gte: 10, $lte:20}})
+    .find({ price: { $in: [10, 15, 20] } })
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
   console.log(courses);
 }
 
-getCourses();
-
+// comparison operators
+// eq, ne, gt, gte, lt, lte, in, nin(not in)
